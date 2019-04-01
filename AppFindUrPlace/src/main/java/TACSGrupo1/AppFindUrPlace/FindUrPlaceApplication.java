38b165eac7ead786485db5b15bd000eb66387556
SantiBalbiani <santiagobalbiani@yahoo.com>
2019-03-31 21:17:04 -0300

    Initial BackEnd SetUp
    Java 11
    Spring
    Spring Data MongoDB
    Maven
    Packaging: JAR

---
+++ AppFindUrPlace/src/main/java/TACSGrupo1/AppFindUrPlace/FindUrPlaceApplication.java
@@ -0,0 +1,13 @@
+package TACSGrupo1.AppFindUrPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindUrPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindUrPlaceApplication.class, args);
+	}
+
+}
